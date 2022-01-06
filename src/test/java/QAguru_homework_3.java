8abb61f7866ef903d2a1153f87862bbf1e6fa512
Weilson1 <galavievai@gmail.com>
2022-01-06 21:52:48 +0300

    Initial commit

---
+++ src/test/java/QAguru_homework_3.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class QAguru_homework_3 {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void SoftAssertionsPageExistanceTest() {
+
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-box").$(byText("Show 2 more pages…")).scrollTo().click();
+//      $("[data-filterable-for=wiki-pages-filter]").$(byText("Show 2 more pages…")).click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $(".markdown-body")
+//      $(byText("Using JUnit5 extend test class:")).scrollTo()
+                .shouldHave(text("Using JUnit5 extend test class:"));
+    }
+    @Test
+    void DragandDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().moveToElement($("#column-b")).clickAndHold()
+                .release().perform();
+        sleep(5000);
+
+
+
+    }
+}
